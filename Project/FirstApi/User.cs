1f030bf374cf6c36eb4c5f7d83bc92d17e8be832
Digdem Cokyasar <51333345+dodididop@users.noreply.github.com>
2021-11-28 01:01:30 +0300

    added data annotation

--- Project/FirstApi/User.cs
+++ Project/FirstApi/User.cs
@@ -1,12 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FirstApi
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(20),MinLength(2)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(25),MinLength(2)]
         public string SurName { get; set; }
+        [Required]
         public string Password { get; set; }
 
     }
